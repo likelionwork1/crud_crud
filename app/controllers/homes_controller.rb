1a9c402e0cc227898de1872d0aad3665505b7ec8
likelionwork1 <ubuntu@ip-172-31-22-75.us-east-2.compute.internal>
2019-07-01 17:02:45 +0000

    crud

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -16,5 +16,31 @@ class HomesController < ApplicationController
     @posts = Post.all
   end
   
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    post = Post.find(params[:id])
+    post.title = params[:title]
+    post.content = params[:content]
+    post.numb = params[:numb]
+    post.save
+    
+    redirect_to "/homes/read"
+  end
+  
+  def delete
+    # post = Post.find(params[:id])
+    # post.destroy
+    Post.destroy(params[:id])
+    
+    redirect_to "/homes/read"
+  end
+  
 end
+
+
+
+
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   get 'homes/second'
   get 'homes/read'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'homes/edit/:id' => 'homes#edit'
+  get 'homes/update/:id' => 'homes#update'
+  get 'homes/delete/:id' => 'homes#delete'
   
   root 'homes#index'
 end
+
+
+
+
